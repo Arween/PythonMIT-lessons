1b48f074b3f898656aba85de3ed867b10736e54f
Arween <semka.alesya@gmail.com>
2017-02-26 13:39:03 +0300

    corrected again

--- Lecture-test/lect2_t3.py
+++ Lecture-test/lect2_t3.py
@@ -1,29 +1,31 @@
-outstandingBalance = float(raw_input("Enter the outstanding balance on your credit card: "));
-interestRate = float(raw_input("Enter the annual credit card interest rate as a decimal: "));
+# outstandingBalance = float(raw_input("Enter the outstanding balance on your credit card: "));
+# interestRate = float(raw_input("Enter the annual credit card interest rate as a decimal: "));
 
+
+# outstandingBalance = 320000.0;
+# interestRate = 0.2
+
+outstandingBalance = 999999;
+interestRate = 0.18
 
 monthlyInterestRate = interestRate/12
 balance = outstandingBalance
 lowerBound = balance / 12.0
 upperBound = (balance * (1 + (interestRate / 12.0)) ** 12.0) / 12.0
+epsilon = 0.2
+monthlyPayment = (upperBound + lowerBound) / 2.0
 
 
-while balance > 0:
+while (balance > epsilon) or (balance < -epsilon):
     monthlyPayment = (upperBound + lowerBound) / 2.0
+    balance = outstandingBalance
     for months in range(1,13):
-        balance = round((balance * (1 + monthlyInterestRate)-monthlyPayment),2)
-        if balance <= 0:
-            break
-    if balance == 0.0:
-        outstandingBalance = balance
-    elif balance > 0.0:
-        balance = outstandingBalance
+        balance = (balance * (1 + (interestRate / 12.0))) - monthlyPayment
+    if balance > epsilon:
         lowerBound = monthlyPayment
-    else:
-        balance = outstandingBalance
+    if balance < -epsilon:
         upperBound = monthlyPayment
 
 print "RESULT";
-print "Monthly payment to pay off debt in 1 year: ", round(monthlyPayment, 2)
-print "Number of months needed: ", months
+print "Monthly payment to pay off debt in 1 year: ", monthlyPayment
 print "Balance: ", round(balance, 2)
